62f616258a8853578ce742abaf03edb433f9ad1e
Mohan <mohanu526@gmail.com>
2020-11-02 03:20:46 +0530

    test comm

--- lib/Screens/vendorProfile.dart
+++ lib/Screens/vendorProfile.dart
@@ -1,231 +1,231 @@
-import 'package:vendorProfile/Animations/FadeAnimation.dart';
-import 'package:flutter/material.dart';
-class VendorProfile extends StatefulWidget {
-  @override
-  _VendorProfileState createState() => _VendorProfileState();
-}
-
-class _VendorProfileState extends State<VendorProfile> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[
-          CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                expandedHeight: 450,
-                backgroundColor: Colors.white,
-                flexibleSpace: FlexibleSpaceBar(
-                  collapseMode: CollapseMode.pin,
-                  background: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/emma.jpg'),
-                            fit: BoxFit.cover)),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                              begin: Alignment.bottomRight,
-                              colors: [
-                                Colors.black,
-                                Colors.black.withOpacity(.3)
-                              ])),
-                      child: Padding(
-                        padding: EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: <Widget>[
-                            FadeAnimation(
-                                1,
-                                Text(
-                                  "Tasveer Photographers",
-                                  style: TextStyle(
-                                      color: Colors.pinkAccent,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 40),
-                                )),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                FadeAnimation(
-                                    1.2,
-                                    Text(
-                                      "4.3 stars",
-                                      style: TextStyle(
-                                          color: Colors.yellow, fontSize: 16),
-                                    )),
-                                SizedBox(
-                                  width: 50,
-                                ),
-                                FadeAnimation(
-                                    1.2,
-                                    Text(
-                                      "265 Ratings",
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 16),
-                                    ))
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SliverList(
-                delegate: SliverChildListDelegate([
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        FadeAnimation(
-                            1.6,
-                            Text(
-                              "We at tasvver Photography beleive in giving you the best possible service we can and we are really proffesional we are a 5 star company we can cover events of weddings to birthdays parties and we are punctual you can count on us for your future endevaours.",
-                              style:
-                              TextStyle(color: Colors.black, height: 1.4),
-                            )),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        FadeAnimation(
-                            1.6,
-                            Text(
-                              "Pricing",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        FadeAnimation(
-                            1.6,
-                            Text(
-                              "₹1000/hr :- no time specified"
-                                  "                                              ₹15,000 :-  weddings",
-                              style: TextStyle(color: Colors.black),
-                            )),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        FadeAnimation(
-                            1.6,
-                            Text(
-                              "Location",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        FadeAnimation(
-                            1.6,
-                            Text(
-                              "Delhi ,  Ghaziabad",
-                              style: TextStyle(color: Colors.black),
-                            )),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        FadeAnimation(
-                            1.6,
-                            Text(
-                              "Work",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        FadeAnimation(
-                            1.8,
-                            Container(
-                              height: 200,
-                              child: ListView(
-                                scrollDirection: Axis.horizontal,
-                                children: <Widget>[
-                                  makeVideo(image: 'assets/images/emma-1.jpg'),
-                                  makeVideo(image: 'assets/images/emma-2.jpg'),
-                                  makeVideo(image: 'assets/images/emma-3.jpg'),
-                                ],
-                              ),
-                            )),
-                        SizedBox(
-                          height: 120,
-                        )
-                      ],
-                    ),
-                  )
-                ]),
-              )
-            ],
-          ),
-          Positioned.fill(
-            bottom: 50,
-            child: Container(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: FadeAnimation(
-                  2,
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 30),
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.pinkAccent[700]),
-                    child: Align(
-                        child: Text("Contact Provider",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ))),
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget makeVideo({image}) {
-    return AspectRatio(
-      aspectRatio: 1.5 / 1,
-      child: Container(
-        margin: EdgeInsets.only(right: 20),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image:
-            DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
-        child: Container(
-          decoration: BoxDecoration(
-              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
-                Colors.black.withOpacity(.9),
-                Colors.black.withOpacity(.3)
-              ])),
-          child: Align(
-            child: Icon(
-              Icons.play_arrow,
-              color: Colors.white,
-              size: 70,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-}
+//import 'package:vendorProfile/Animations/FadeAnimation.dart';
+//import 'package:flutter/material.dart';
+//class VendorProfile extends StatefulWidget {
+//  @override
+//  _VendorProfileState createState() => _VendorProfileState();
+//}
+//
+//class _VendorProfileState extends State<VendorProfile> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Colors.white,
+//      body: Stack(
+//        children: <Widget>[
+//          CustomScrollView(
+//            slivers: <Widget>[
+//              SliverAppBar(
+//                expandedHeight: 450,
+//                backgroundColor: Colors.white,
+//                flexibleSpace: FlexibleSpaceBar(
+//                  collapseMode: CollapseMode.pin,
+//                  background: Container(
+//                    decoration: BoxDecoration(
+//                        image: DecorationImage(
+//                            image: AssetImage('assets/images/emma.jpg'),
+//                            fit: BoxFit.cover)),
+//                    child: Container(
+//                      decoration: BoxDecoration(
+//                          gradient: LinearGradient(
+//                              begin: Alignment.bottomRight,
+//                              colors: [
+//                                Colors.black,
+//                                Colors.black.withOpacity(.3)
+//                              ])),
+//                      child: Padding(
+//                        padding: EdgeInsets.all(20),
+//                        child: Column(
+//                          crossAxisAlignment: CrossAxisAlignment.start,
+//                          mainAxisAlignment: MainAxisAlignment.end,
+//                          children: <Widget>[
+//                            FadeAnimation(
+//                                1,
+//                                Text(
+//                                  "Tasveer Photographers",
+//                                  style: TextStyle(
+//                                      color: Colors.pinkAccent,
+//                                      fontWeight: FontWeight.bold,
+//                                      fontSize: 40),
+//                                )),
+//                            SizedBox(
+//                              height: 20,
+//                            ),
+//                            Row(
+//                              children: <Widget>[
+//                                FadeAnimation(
+//                                    1.2,
+//                                    Text(
+//                                      "4.3 stars",
+//                                      style: TextStyle(
+//                                          color: Colors.yellow, fontSize: 16),
+//                                    )),
+//                                SizedBox(
+//                                  width: 50,
+//                                ),
+//                                FadeAnimation(
+//                                    1.2,
+//                                    Text(
+//                                      "265 Ratings",
+//                                      style: TextStyle(
+//                                          color: Colors.white, fontSize: 16),
+//                                    ))
+//                              ],
+//                            )
+//                          ],
+//                        ),
+//                      ),
+//                    ),
+//                  ),
+//                ),
+//              ),
+//              SliverList(
+//                delegate: SliverChildListDelegate([
+//                  Padding(
+//                    padding: EdgeInsets.all(20),
+//                    child: Column(
+//                      crossAxisAlignment: CrossAxisAlignment.start,
+//                      children: <Widget>[
+//                        FadeAnimation(
+//                            1.6,
+//                            Text(
+//                              "We at tasvver Photography beleive in giving you the best possible service we can and we are really proffesional we are a 5 star company we can cover events of weddings to birthdays parties and we are punctual you can count on us for your future endevaours.",
+//                              style:
+//                              TextStyle(color: Colors.black, height: 1.4),
+//                            )),
+//                        SizedBox(
+//                          height: 40,
+//                        ),
+//                        FadeAnimation(
+//                            1.6,
+//                            Text(
+//                              "Pricing",
+//                              style: TextStyle(
+//                                  color: Colors.black,
+//                                  fontSize: 18,
+//                                  fontWeight: FontWeight.bold),
+//                            )),
+//                        SizedBox(
+//                          height: 10,
+//                        ),
+//                        FadeAnimation(
+//                            1.6,
+//                            Text(
+//                              "₹1000/hr :- no time specified"
+//                                  "                                              ₹15,000 :-  weddings",
+//                              style: TextStyle(color: Colors.black),
+//                            )),
+//                        SizedBox(
+//                          height: 20,
+//                        ),
+//                        FadeAnimation(
+//                            1.6,
+//                            Text(
+//                              "Location",
+//                              style: TextStyle(
+//                                  color: Colors.black,
+//                                  fontSize: 18,
+//                                  fontWeight: FontWeight.bold),
+//                            )),
+//                        SizedBox(
+//                          height: 10,
+//                        ),
+//                        FadeAnimation(
+//                            1.6,
+//                            Text(
+//                              "Delhi ,  Ghaziabad",
+//                              style: TextStyle(color: Colors.black),
+//                            )),
+//                        SizedBox(
+//                          height: 20,
+//                        ),
+//                        FadeAnimation(
+//                            1.6,
+//                            Text(
+//                              "Work",
+//                              style: TextStyle(
+//                                  color: Colors.black,
+//                                  fontSize: 18,
+//                                  fontWeight: FontWeight.bold),
+//                            )),
+//                        SizedBox(
+//                          height: 20,
+//                        ),
+//                        FadeAnimation(
+//                            1.8,
+//                            Container(
+//                              height: 200,
+//                              child: ListView(
+//                                scrollDirection: Axis.horizontal,
+//                                children: <Widget>[
+//                                  makeVideo(image: 'assets/images/emma-1.jpg'),
+//                                  makeVideo(image: 'assets/images/emma-2.jpg'),
+//                                  makeVideo(image: 'assets/images/emma-3.jpg'),
+//                                ],
+//                              ),
+//                            )),
+//                        SizedBox(
+//                          height: 120,
+//                        )
+//                      ],
+//                    ),
+//                  )
+//                ]),
+//              )
+//            ],
+//          ),
+//          Positioned.fill(
+//            bottom: 50,
+//            child: Container(
+//              child: Align(
+//                alignment: Alignment.bottomCenter,
+//                child: FadeAnimation(
+//                  2,
+//                  Container(
+//                    margin: EdgeInsets.symmetric(horizontal: 30),
+//                    height: 50,
+//                    decoration: BoxDecoration(
+//                        borderRadius: BorderRadius.circular(50),
+//                        color: Colors.pinkAccent[700]),
+//                    child: Align(
+//                        child: Text("Contact Provider",
+//                            style: TextStyle(
+//                              color: Colors.white,
+//                            ))),
+//                  ),
+//                ),
+//              ),
+//            ),
+//          )
+//        ],
+//      ),
+//    );
+//  }
+//
+//  Widget makeVideo({image}) {
+//    return AspectRatio(
+//      aspectRatio: 1.5 / 1,
+//      child: Container(
+//        margin: EdgeInsets.only(right: 20),
+//        decoration: BoxDecoration(
+//            borderRadius: BorderRadius.circular(20),
+//            image:
+//            DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+//        child: Container(
+//          decoration: BoxDecoration(
+//              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+//                Colors.black.withOpacity(.9),
+//                Colors.black.withOpacity(.3)
+//              ])),
+//          child: Align(
+//            child: Icon(
+//              Icons.play_arrow,
+//              color: Colors.white,
+//              size: 70,
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
+//}
